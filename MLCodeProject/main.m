3d4adedbdf79eb5adddcaf3c57c5f4ede7316350
luomagaoshou <miaolonglai@miaolonglaideMacBook-Pro.local>
2017-05-24 14:47:51 +0800

    first commit

---
+++ MLCodeProject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MLCodeProject
+//
+//  Created by 妙龙赖 on 2017/5/24.
+//  Copyright © 2017年 妙龙赖. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
